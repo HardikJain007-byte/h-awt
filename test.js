51c3088ef4d5899f51eb73955e940d90ac4d9709
HardikJain007-byte <Royhardik04@gmail.com>
2025-10-11 18:44:23 +0530

    commit

---
+++ test.js
@@ -0,0 +1,15 @@
+var name ="Hardik "
+
+var email = "Hjain2045@gmail.com"
+
+console.log("Name:"+name)
+console.log("Email"+email)
+
+var a = 10
+var b = 20
+
+var sum = a+b
+console.log("Sum: "+sum)
+
+var diff =b-a
+console.log("Diff: "+diff)
